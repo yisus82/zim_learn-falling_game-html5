4da3997f70d8935a91703cbb95be62f4aa456abf
yisus82 <djalma8@gmail.com>
2024-12-02 01:41:57 +0100

    Add falling pods

--- js/game.js
+++ js/game.js
@@ -1,4 +1,41 @@
-const playGame = () => {};
+const playGame = () => {
+  // Pods
+  // Every DisplayObject might be interacted with
+  // Adding noMouse() we are saying we will not interact with it and takes less processing
+  // It would be fine without it, but it could help on older mobiles
+  const pods = new Container().addTo().noMouse();
+  interval(
+    {
+      min: 0.2,
+      max: 0.5,
+    },
+    () => {
+      new Circle({
+        radius: 20,
+        color: [yellow, red, blue, silver, grey, dark],
+      })
+        .loc(rand(W), -100, pods)
+        .animate({
+          // A string value is relative position,
+          // so animate to left or right 100 from its current x
+          // and then off the screen to the bottom
+          props: {
+            x: String(rand(-100, 100)),
+            y: H + 100,
+          },
+          ease: 'linear',
+          time: {
+            min: 2,
+            max: 5,
+          },
+          call: target => {
+            // Dispose what just stopped animating
+            target.dispose();
+          },
+        });
+    }
+  );
+};
 
 const ready = () => {
   new Pane({
